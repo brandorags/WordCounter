f118b9c96780c1f0dfc5fdf2723e07566b282440
brandorags <brandorags@users.noreply.github.com>
2015-09-19 17:54:56 -0600

    added license and cleared up comments

--- python/word_counter_console.py
+++ python/word_counter_console.py
@@ -1,9 +1,28 @@
 """
 Name: word_counter_console.py
 Author: Brandon Ragsdale
-Date: 9/17/2015
 Description: This script counts the number of times
 a particular word occurs within a set of files.
+
+Copyright (c) 2015 Brandon Ragsdale
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 
 from pathlib import Path
@@ -17,9 +36,6 @@ try:
     # check if the user entered the path of the
     # directory and at least one file type
     if len(args) >= 3:
-        # retrieve the argument that contains
-        # the directory path from the command line
-        # and initialize the counter
         directory_path = args[1]
         word = args[2]
 
@@ -35,8 +51,6 @@ try:
             # if the file type is found in the
             # command line arguments, open the file
             if file_type in args:
-                # open the file and show the
-                # file path to the user
                 f = open(str(directory_file))
                 directory_file_name = str(directory_file)
                 print(directory_file_name + ":")
@@ -51,11 +65,11 @@ try:
                     word_counter += 1
                     print(word + " found at line " + str(line_counter) + ".")
 
-                # display the total number of times the word
-                # was found in the file
+                # display the total number of times
+                # the word was found in the file
                 print(word + " was found " + str(word_counter) + " times.")
     else:
-        # if they didn't put any arguments in,
+        # if the user didn't put any arguments in,
         # let them know how to use the script
         print('Usage: python3 word_counter_console.py [path_of_directory] [word] [file_type] [file_type] [etc.]')
         print('Example: python3 word_counter_console.py ~/Documents wordToSearch txt log')
